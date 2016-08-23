d4d0fa2626927c31f88dceecce2300ef07e29257
Sebastián Salazar Molina <sebasalazar@gmail.com>
2016-08-23 15:26:03 -0300

    Ajustes para obtener información

--- src/main/java/cl/medipacs/pacs/cli/service/impl/DbServiceImpl.java
+++ src/main/java/cl/medipacs/pacs/cli/service/impl/DbServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -45,6 +46,7 @@ public class DbServiceImpl implements DbService, Serializable {
                     if (StringUtils.isNotBlank(data)) {
                         LOGGER.info(String.format("# ID: '%d' # Datos: '%s' #", id, data));
                         attributes.add(data);
+                        DataUtils.createDcmFile(id, attrs);
                     }
                 }
                 rs.close();
--- src/main/java/cl/medipacs/pacs/cli/utils/DataUtils.java
+++ src/main/java/cl/medipacs/pacs/cli/utils/DataUtils.java
@@ -1,7 +1,11 @@
 package cl.medipacs.pacs.cli.utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,5 +34,25 @@ public class DataUtils implements Serializable {
             LOGGER.error("Error al serializar datos: {}", e.toString());
         }
         return result;
+    }
+
+    public static File createDcmFile(Long id, byte[] data) {
+        File file = null;
+        try {
+            if (id != null && data != null) {
+                String directory = String.format("%s%sdata", System.getProperty("java.io.tmpdir"), System.getProperty("file.separator"));
+                File dir = new File(directory);
+                dir.mkdirs();
+
+                file = new File(String.format("%s%s%d.dcm", directory, System.getProperty("file.separator"), id));
+                OutputStream out = new FileOutputStream(file);
+                IOUtils.write(data, out);
+                out.close();
+            }
+        } catch (Exception e) {
+            file = null;
+            LOGGER.error("Error al crear archivo dcm: {}", e.toString());
+        }
+        return file;
     }
 }
